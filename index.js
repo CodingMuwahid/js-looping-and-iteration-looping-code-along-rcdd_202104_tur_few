69a125f30cd9f8de5835300f51fd4d2678404e84
Isa Tekinkaya <isatekinkaya@yahoo.de>
2021-06-21 16:36:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Code your solutions in this file
+const names = ["Ada", "Brendan", "Ali"]
+const eventt = ["for the wonderful prayer!"];
+
+function writeCards(namess, eventtt){
+  for(i = 0; i < namess.length; i++){
+    
+   } let arrayy =[`Thank you, ${namess[i]} for the ${eventtt}`]
+} writeCards(names, eventt)
